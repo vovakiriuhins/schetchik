f510f2f3f0b0c25a7841aebd1af7fcc41d4974a7
vovakiriuhins <vova11999977@gmail.com>
2023-04-09 17:14:25 +0300

    Counter, useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {Counter} from "./Counter";
+import {v1} from "uuid";
+import s from "./Counter.module.css";
+
+// export type TitlesPropsType = {
+//     id: string
+//     title: string
+// }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const [value, setValue] = useState<number>(() => {
+        return Number(localStorage.getItem('counterValue')) || 0
+    })
+
+
+    useEffect(() => {
+        let valueAsString = localStorage.getItem("counterValue")
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setValue(newValue)
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem("counterValue", JSON.stringify(value))
+    }, [value])
+
+
+    const incrHandler = () => {
+        setValue(value + 1)
+    }
+
+    const resetHandler = () => {
+        setValue(0)
+    }
+
+
+
+    return (
+        <div className="App">
+            <div>
+                {/*<h1>{value}</h1>*/}
+                {/*  <button onClick={incrHandler}>Incr</button>*/}
+                {/*  <button onClick={resetHandler}>Reset</button>*/}
+                <Counter
+                    value={value}
+                    incrHandler={incrHandler}
+                    resetHandler={resetHandler}
+                />
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
